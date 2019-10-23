ecd18f8f36f495d9635a40ef745001372ddb3153
coconutZzx <naylorsydney@gmail.com>
2019-10-23 10:54:01 +0200

    UI Menu/ Map Size
    Completed Menu with working buttons and map size changes using sliders

---
+++ GADE-FinalPOE-SydneyNaylor-19013037/Assets/Scripts/MapManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapManager : MonoBehaviour
+{
+    public Slider sliderWidth;
+    public Slider sliderLength;
+
+    public static int width;
+    public static int length;
+
+    public static bool inMenu = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inMenu)
+        {
+            width = (int)sliderWidth.value;
+            length = (int)sliderLength.value;
+        }
+    }
+}
---
+++ GADE-FinalPOE-SydneyNaylor-19013037/Assets/Scripts/MapScale.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(MapManager.width, MapManager.length, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
