a23e5fc0efd8dd6e6a455c008cf633b13ee36929
coconutZzx <naylorsydney@gmail.com>
2019-10-27 02:30:51 +0200

    Units Randomly Spawn
    Wizards, Melee and ranged randomly spawn in and have different colour for their different teams.

---
+++ GADE-FinalPOE-SydneyNaylor-19013037/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject unit;
+    public int x;
+    public int z;
+    public int unitAmount;
+    void Start()
+    {
+        StartCoroutine(UnitSpawner());
+    }
+
+    IEnumerator UnitSpawner()
+    {
+        while (unitAmount < 3)
+        {
+            x = Random.Range(-MapManager.width/2, MapManager.width/2);
+            z = Random.Range(-MapManager.length/2, MapManager.length/2);
+            Instantiate(unit, new Vector3(x, 1, z), Quaternion.identity);
+            yield return new WaitForSeconds(0.0f);
+            unitAmount += 1;
+        }
+    }
+
+}
---
+++ GADE-FinalPOE-SydneyNaylor-19013037/Assets/Scripts/WizardSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardSpawner : MonoBehaviour
+{
+    public GameObject unit;
+    public int x;
+    public int z;
+    public int unitAmount;
+    void Start()
+    {
+        StartCoroutine(UnitSpawner());
+    }
+
+    IEnumerator UnitSpawner()
+    {
+        while (unitAmount < 6)
+        {
+            x = Random.Range(-MapManager.width / 2, MapManager.width / 2);
+            z = Random.Range(-MapManager.length / 2, MapManager.length / 2);
+            Instantiate(unit, new Vector3(x, 1, z), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            unitAmount += 1;
+        }
+    }
+}
