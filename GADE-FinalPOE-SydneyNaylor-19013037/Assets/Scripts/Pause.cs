a89215feaa64a9346d61d5c413336d0a53c41d86
coconutZzx <naylorsydney@gmail.com>
2019-10-28 14:05:10 +0200

    Pause Menu
    Game pauses and unpauses using escape button.

---
+++ GADE-FinalPOE-SydneyNaylor-19013037/Assets/Scripts/Pause.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    //bool isPaused;
+
+    //public void PauseGame()
+    //{
+    //    if (isPaused)
+    //    {
+    //        isPaused = false;
+    //        Time.timeScale = 1;
+    //    }
+    //    if (!isPaused)
+    //    {
+    //        isPaused = true;
+    //        Time.timeScale = 0;
+    //    }
+    //}
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    
+    void Paused()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}
